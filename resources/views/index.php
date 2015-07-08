d6b610836d74668a9d2cf9eef12f70277d47937b
NinjaGrisen <wallof.kristofer@gmail.com>
2015-07-08 15:23:57 +0200

    commits

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,6 +5,7 @@
     <!-- CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"> <!-- load bootstrap via cdn -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"> <!-- load fontawesome -->
+    <link rel="stylesheet" href="css/main.css">
     <style>
         body        { padding-top:30px; }
         form        { padding-bottom:20px; }
