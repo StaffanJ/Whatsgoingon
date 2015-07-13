ff1e3d629415d0106d47fea55b44edd14dbffb69
NinjaGrisen <wallof.kristofer@gmail.com>
2015-07-13 14:06:54 +0200

    localbootstrap
    Bra att ha om internet fuckar xD

--- resources/views/index.php
+++ resources/views/index.php
@@ -7,6 +7,8 @@
 <title>What's Going On!</title>
 
     <!-- CSS -->
+<!--     <link rel="stylesheet" type="text/css" href="css/bootstrap-3.3.5-dist/css/bootstrap.css">
+ -->    
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"> <!-- load bootstrap via cdn -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css"> <!-- load fontawesome -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
